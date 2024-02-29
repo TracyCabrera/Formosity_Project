f6d268c1de6c3a466a8b20957b4d13318172168b
siyakiya <shma.javier.up@phinmaed.com>
2024-02-29 19:43:33 +0800

    db

--- app/src/main/java/com/application/formosity_project/signin__login.kt
+++ app/src/main/java/com/application/formosity_project/signin__login.kt
@@ -1,11 +1,12 @@
 package com.application.formosity_project
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 
-class signin__login : AppCompatActivity() {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class  signin__login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin_login)
+            }
     }
-}
